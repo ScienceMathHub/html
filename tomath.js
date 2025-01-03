6be99edfa31054ec3ec75aa04a2054a9cbba5c76
ScienceMathHub <98516276+ScienceMathHub@users.noreply.github.com>
2025-01-03 23:18:33 +0900

    Add files via upload

---
+++ tomath.js
@@ -0,0 +1,18 @@
+/*
+------------------------------------------------------------------------
+ To Mathematics
+
+ HTML(JavaScript)
+
+ (c)Ohtani 2025.1
+------------------------------------------------------------------------
+*/
+document.write('<pre></pre>');
+document.write('<h4 id=index>');
+document.write('<div class = "color_black">');
+document.write('<a href="https://sciencemathhub.github.io/html/math.html"');
+document.write('rel="noopener noreferrer" target="_self">');
+document.write('Exit');
+document.write('</a>');
+document.write('</div>');
+document.write('</h3>');
