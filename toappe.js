412be6341abd47bd90c679de70bd8b2ab2bb4047
ScienceMathHub <98516276+ScienceMathHub@users.noreply.github.com>
2025-01-15 03:19:33 +0900

    Add files via upload

--- toappe.js
+++ toappe.js
@@ -9,7 +9,7 @@
 */
 //document.write('<h4 id=index>');
 //document.write('<div class = "color_black">');
-document.write('<a href="https://sciencemathhub.github.io/html/paae.html"');
+document.write('<a href="https://sciencemathhub.github.io/html/appe.html"');
 document.write('rel="noopener noreferrer" target="_self">');
 document.write('Exit');
 document.write('</a>');
