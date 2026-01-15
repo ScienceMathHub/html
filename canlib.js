21116fe3b0a7346bf5d290f7412a5e09426672e3
ScienceMathHub <98516276+ScienceMathHub@users.noreply.github.com>
2026-01-15 23:01:33 +0900

    Add files via upload

--- canlib.js
+++ canlib.js
@@ -4,7 +4,7 @@
 
  HTML(JavaScript, canvas)
 
- (c)Ohtani 2024.4 - 2025.8
+ (c)Ohtani 2024.4 - 2026.1
 ------------------------------------------------------------------------
 */
 'use strict'
@@ -229,6 +229,79 @@ class BASIC
         continue;
       }
       if (num && (a == 'e' || a == 'E'))
+      {
+        this.PRINT("×10");
+        this.FONT(0.7, -0.4);
+        i++;
+        a = s.substring(i, i+1);
+        if      (a == '+') { i++;                     }
+        else if (a == '-') { i++; this.PRINT(a); p++; }
+        for (;i < n; i++)
+        {
+          a = s.substring(i, i+1);
+          if (!('0' <= a && a <= '9')) { i--; break; }
+          this.PRINT(a); p++;
+        }
+        num = 0;
+        this.FONT(1, 0);
+        continue;
+      }
+      num = ('0' <= a && a <= '9')? 1 : 0;
+      this.PRINT(a);
+    }
+    this.FONT(1, 0);
+    return p;	/* 0.7の数 */
+  }
+  PRINTE(s)
+  {
+    let i, n, num, a, p;
+
+    s = String(s);
+    p = 0;
+    num = 0;
+    n = s.length;
+    for (i = 0; i < n; i++)
+    {
+      a = s.substring(i, i+1);
+      if (a == '^')
+      {
+        i++;
+        a = s.substring(i, i+1);
+        if (a == "'")
+        {
+          this.LocX += this.LocW * 0.3;
+          continue;
+        }
+        if (a == "`")
+        {
+          this.LocX -= this.LocW * 0.3;
+          continue;
+        }
+        this.FONT(0.7, -0.3);
+        this.PRINT(a); p++;
+        this.FONT(1, 0);
+        continue;
+      }
+      if (a == '_')
+      {
+        i++;
+        a = s.substring(i, i+1);
+        if (a == "'")
+        {
+          this.LocX += this.LocW * 0.5;
+          continue;
+        }
+        if (a == "`")
+        {
+          this.LocX -= this.LocW * 0.5;
+          continue;
+        }
+        this.FONT(0.7, 0.1);
+        this.PRINT(a); p++;
+        this.FONT(1, 0);
+        continue;
+      }
+      if (num && a == 'E')
       {
         this.PRINT("×10");
         this.FONT(0.7, -0.4);
