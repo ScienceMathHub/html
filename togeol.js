10d3b8842b4911dfc728066444f1d379903f2624
ScienceMathHub <98516276+ScienceMathHub@users.noreply.github.com>
2026-04-29 00:18:59 +0900

    Add files via upload

--- togeol.js
+++ togeol.js
@@ -7,12 +7,12 @@
  (c)Ohtani 2026.4
 ------------------------------------------------------------------------
 */
-document.write('<pre></pre>');
-document.write('<h4 id=index>');
-document.write('<div class = "color_black">');
+//document.write('<pre></pre>');
+//document.write('<h4 id=index>');
+//document.write('<div class = "color_black">');
 document.write('<a href="https://sciencemathhub.github.io/html/geol.html"');
 document.write('rel="noopener noreferrer" target="_self">');
 document.write('Exit');
 document.write('</a>');
-document.write('</div>');
-document.write('</h4>');
+//document.write('</div>');
+//document.write('</h4>');
